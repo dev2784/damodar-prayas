4419f05e9053f6c4c715c294cb47faa45968e117
dev2784 <devendraxtra@gmail.com>
2026-09-14 17:28:57 +0530

    Add JWT plugin foundation

---
+++ apps/api/src/plugins/auth.ts
@@ -0,0 +1,9 @@
+import jwt from '@fastify/jwt';
+import type { FastifyInstance } from 'fastify';
+import { env } from '../config/env.js';
+
+export async function authPlugin(app: FastifyInstance) {
+  await app.register(jwt, {
+    secret: env.JWT_SECRET,
+  });
+}
