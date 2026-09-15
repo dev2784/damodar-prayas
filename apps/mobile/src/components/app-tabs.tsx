22e9c5b2a590eeee9aaa09d6eab81863deae097e
dev2784 <devendraxtra@gmail.com>
2026-09-15 20:23:45 +0530

    fix(mobile): hide matrimony interaction screens from bottom tabs

--- apps/mobile/src/components/app-tabs.tsx
+++ apps/mobile/src/components/app-tabs.tsx
@@ -79,6 +79,8 @@ export default function AppTabs() {
       <Tabs.Screen name="matrimony-profile" options={{ href: null }} />
       <Tabs.Screen name="my-matrimony" options={{ href: null }} />
       <Tabs.Screen name="matrimony-form" options={{ href: null }} />
+      <Tabs.Screen name="matrimony-shortlist" options={{ href: null }} />
+      <Tabs.Screen name="matrimony-interests" options={{ href: null }} />
       <Tabs.Screen name="auth" options={{ href: null }} />
     </Tabs>
   );
