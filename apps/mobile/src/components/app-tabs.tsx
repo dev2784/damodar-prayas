c2cc2d7ee259701bb22c8ce51d6962e3452656ec
dev2784 <devendraxtra@gmail.com>
2026-09-15 21:35:50 +0530

    feat(committees): hide committee detail route from tabs

--- apps/mobile/src/components/app-tabs.tsx
+++ apps/mobile/src/components/app-tabs.tsx
@@ -77,6 +77,7 @@ export default function AppTabs() {
       />
       <Tabs.Screen name="explore" options={{ href: null }} />
       <Tabs.Screen name="community-post" options={{ href: null }} />
+      <Tabs.Screen name="samiti-detail" options={{ href: null }} />
       <Tabs.Screen name="matrimony-profile" options={{ href: null }} />
       <Tabs.Screen name="my-matrimony" options={{ href: null }} />
       <Tabs.Screen name="matrimony-form" options={{ href: null }} />
