b6199666045843f19cdff85935cad39ff2cce3c3
dev2784 <devendraxtra@gmail.com>
2026-09-21 18:30:54 +0530

    fix(mobile): hide FAQ from bottom tab navigation

--- apps/mobile/src/components/app-tabs.tsx
+++ apps/mobile/src/components/app-tabs.tsx
@@ -84,6 +84,7 @@ export default function AppTabs() {
       <Tabs.Screen name="notifications" options={{ href: null }} />
       <Tabs.Screen name="change-password" options={{ href: null }} />
       <Tabs.Screen name="settings" options={{ href: null }} />
+      <Tabs.Screen name="faq" options={{ href: null }} />
       <Tabs.Screen name="auth" options={{ href: null }} />
     </Tabs>
   );
